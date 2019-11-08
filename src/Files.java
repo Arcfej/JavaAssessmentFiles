fa284d39aa3b9a492d8293b517726e142a12b90d
MiklosMayer <MiklosMayer@computing.dundee.ac.uk>
2019-11-08 18:04:02 +0000

    Initial commit

---
+++ src/Files.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author MiklosMayer
+ *
+ */
+public class Files {
+
+	/**
+	 * 
+	 */
+	public Files() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
